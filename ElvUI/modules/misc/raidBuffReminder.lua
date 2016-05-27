882534d978f95e898c42cbabe2c195c5e4cc6d84
Appolyonn <jellefenxd@gmail.com>
2016-05-27 10:45:35 +0300

    Update Reminder
    Spells Update

--- ElvUI/modules/misc/raidBuffReminder.lua
+++ ElvUI/modules/misc/raidBuffReminder.lua
@@ -1,88 +1,91 @@
-local E, L, V, P, G = unpack(select(2, ...));
+﻿local E, L, V, P, G = unpack(select(2, ...));
 local RB = E:NewModule('ReminderBuffs', 'AceEvent-3.0');
 local LSM = LibStub('LibSharedMedia-3.0');
 
 E.ReminderBuffs = RB;
 
 RB.Spell1Buffs = {
-	67016, --'Flask of the North-SP'
-	67017, --'Flask of the North-AP'
-	67018, --'Flask of the North-STR'
-	53758, --'Flask of Stoneblood'
-	53755, --'Flask of the Frost Wyrm',
-	54212, --'Flask of Pure Mojo',
-	53760, --'Flask of Endless Rage',
-	17627, --'Flask of Distilled Wisdom',
-	
-	33721, --'Spellpower Elixir',
-	53746, --'Wrath Elixir',
-	28497, --'Elixir of Mighty Agility',
-	53748, --'Elixir of Mighty Strength',
-	60346, --'Elixir of Lightning Speed',
-	60344, --'Elixir of Expertise',
-	60341, --'Elixir of Deadly Strikes',
-	60345, --'Elixir of Armor Piercing',
-	60340, --'Elixir of Accuracy',
-	53749, --'Guru's Elixir',
-	
-	60343, --'Elixir of Mighty Defense',
-	53751, --'Elixir of Mighty Fortitude',
-	53764, --'Elixir of Mighty Mageblood',
-	60347, --'Elixir of Mighty Thoughts',
-	53763, --'Elixir of Protection',
-	53747, --'Elixir of Spirit',
+	94160, -- 'Flask of Flowing Water',
+	79470, -- 'Flask of the Draconic Mind',
+	79471, -- 'Flask of the Winds',
+	79472, -- 'Flask of Titanic Strength',
+	79638, -- 'Flask of Enhancement-STR',
+	79639, -- 'Flask of Enhancement-AGI',
+	79640, -- 'Flask of Enhancement-INT',
+	92679, -- 'Flask of Battle',
+	79469, -- 'Flask of Steelskin',
+	79481, -- 'Hit',
+	79632, -- 'Haste',
+	79477, -- 'Critical',
+	79635, -- 'Mastery',
+	79474, -- 'Expertise',
+	79468, -- 'Spirit',
+	79480, -- 'Armor',
+	79631, -- 'Resistance+90',
 };
 
 RB.Spell2Buffs = {
-	57325, -- 80 Силы атаки;
-	57327, -- 46 Силы заклинаний;
-	57329, -- 40 Критического удара;
-	57332, -- 40 Скорости;
-	57334, -- 20 Восполнение маны;
-	57356, -- 40 Мастерства;
-	57358, -- 40 Пробивание брони;
-	57360, -- 40 Меткости;
-	57363, -- Выслеживание гуманойдов;
-	57365, -- 40 Духа;
-	57367, -- 40 Ловкости;
-	57371, -- 40 Силы;
-	57373, -- Выслеживание животных;
-	57399, -- 80 Силы, 46 Силы заклинаний (Рыбный пир);
-	59230, -- 40 Рейтинг уклонений;
-	65247 -- 20 Силы;
+	87545, -- 90 STR
+	87546, -- 90 AGI
+	87547, -- 90 INT
+	87548, -- 90 SPI
+	87549, -- 90 MAST
+	87550, -- 90 HIT
+	87551, -- 90 CRIT
+	87552, -- 90 HASTE
+	87554, -- 90 DODGE
+	87555, -- 90 PARRY
+	87635, -- 90 EXP
+	87556, -- 60 STR
+	87557, -- 60 AGI
+	87558, -- 60 INT
+	87559, -- 60 SPI
+	87560, -- 60 MAST
+	87561, -- 60 HIT
+	87562, -- 60 CRIT
+	87563, -- 60 HASTE
+	87564, -- 60 DODGE
+	87634, -- 60 EXP
+	87554, -- Seafood Feast
 };
 
 RB.Spell3Buffs = {
-	48469, -- Знак дикой природы;
-	72588 -- Дар дикой природы;
+	1126, -- Mark of the wild
+	90363, -- Embrace of the Shale Spider
+	20217, -- Greater Blessing of Kings
 };
 
 RB.Spell4Buffs = {
-	20217, -- Благословение королей;
-	25898 -- Великое благословение королей;
+	469, -- Commanding
+	6307, -- Blood Pact
+	90364, -- Qiraji Fortitude
+	72590, -- Drums of fortitude
+	21562, -- Fortitude
 };
 
 RB.CasterSpell5Buffs = {
-	42995, -- Чародейский интелект;
-	43002, -- Чародейская гениальность;
-	61316 -- Чародейская гениальность Даларана;
+	61316, -- Dalaran Brilliance (6% SP)
+	1459, -- Arcane Brilliance (6% SP)
 };
 
 RB.MeleeSpell5Buffs = {
-	48161, -- Слово силы: Стойкость;
-	48162 -- Молитва стойкости;
+	6673, -- Battle Shout
+	57330, -- Horn of Winter
+	93435, -- Roar of Courage
+	8076, -- Strength of Earth
 };
 
 RB.CasterSpell6Buffs = {
-	48936, -- Благословение мудрости;
-	48938, -- Великое благословение мудрости;
-	58777 -- Источник маны;
+	5675, -- Mana Spring Totem
+	54424, -- Fel Intelligence
+	19740, -- Blessing of Might
 };
 
 RB.MeleeSpell6Buffs = {
-	48932, -- Благословение могущества;
-	48934, -- Великое благословение могущества;
-	47436 -- Боевой крик;
+	19740, -- Blessing of Might
+	30808, -- Unleashed Rage"
+	53138, -- Abom Might
+	19506, -- Trushot
 };
 
 function RB:CheckFilterForActiveBuff(filter)
@@ -280,9 +283,9 @@ function RB:Initialize()
 		[1] = 'Interface\\Icons\\INV_Potion_97',
 		[2] = 'Interface\\Icons\\Spell_Misc_Food',
 		[3] = 'Interface\\Icons\\Spell_Nature_Regeneration',
-		[4] = 'Interface\\Icons\\Spell_Magic_GreaterBlessingofKings',
-		[5] = (E.Role == 'Caster' and 'Interface\\Icons\\Spell_Holy_MagicalSentry') or 'Interface\\Icons\\Spell_Holy_WordFortitude',
-		[6] = (E.Role == 'Caster' and 'Interface\\Icons\\Spell_Holy_GreaterBlessingofWisdom') or 'Interface\\Icons\\Ability_Warrior_BattleShout'
+		[4] = 'Interface\\Icons\\Spell_Holy_WordFortitude',
+		[5] = (E.Role == 'Caster' and 'Interface\\Icons\\Spell_Holy_MagicalSentry') or 'Interface\\Icons\\Ability_Warrior_BattleShout',
+		[6] = (E.Role == 'Caster' and 'Interface\\Icons\\Spell_Holy_GreaterBlessingofKings') or 'Interface\\Icons\\Spell_Holy_GreaterBlessingofKings'
 	};
 	
 	local frame = CreateFrame('Frame', 'ElvUI_ReminderBuffs', Minimap);
