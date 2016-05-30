9808b53c8d40ad9b9d1ec843f4a5665f1f3d30a3
Appolyonn <jellefenxd@gmail.com>
2016-05-31 00:53:27 +0300

    Update Boss Button
    Needs Test but i think it should work.

--- ElvUI/modules/actionbars/extra.lua
+++ ElvUI/modules/actionbars/extra.lua
@@ -34,11 +34,24 @@ function AB:SetupExtraButton()
 	
 	for i=1, ExtraActionBarFrame:GetNumChildren() do
 		if _G["ExtraActionButton"..i] then
+			_G["ExtraActionButton"..i]:StripTextures()
 			_G["ExtraActionButton"..i].noResize = true;
+			_G["ExtraActionButton"..i].pushed = true;
+			_G["ExtraActionButton"..i].checked = true;
 			self:StyleButton(_G["ExtraActionButton"..i])
+			_G["ExtraActionButton"..i]:SetTemplate()
+			_G["ExtraActionButton"..i..'Icon']:SetDrawLayer('ARTWORK')
+			local tex = _G["ExtraActionButton"..i]:CreateTexture(nil, 'OVERLAY')
+			tex:SetTexture(0.9, 0.8, 0.1, 0.3)
+			tex:SetInside()
+			_G["ExtraActionButton"..i]:SetCheckedTexture(tex)
 		end
 	end
 	
+	if HasExtraActionBar() then
+		ExtraActionBarFrame:Show();
+	end
+
 	AB:Extra_SetAlpha()
 	AB:Extra_SetScale()
 	E:CreateMover(holder, 'BossButton', L["Boss Button"], nil, nil, nil, 'ALL,ACTIONBARS');
