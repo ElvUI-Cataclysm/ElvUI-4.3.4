0884cd3b7f996bbbdd30019d334a48ed1ad0af3e
Appolyonn <jellefenxd@gmail.com>
2016-07-09 03:07:38 +0300

    Moved to Separate Repositories for Better Management.
    ElvUI_AddOnSkins -- Moved
    ElvUI_BagControl -- Moved
    ElvUI_DataTextColors -- Moved
    ElvUI_Microbar -- Moved
    ElvUI_SquareMinimapButtons -- Moved

--- ElvUI_Microbar/locales/english.lua
+++
@@ -1,9 +0,0 @@
-﻿-- English localization file for enGB.
-local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
-local L = AceLocale:NewLocale("ElvUI", "enUS", true);
-if not L then return; end
-
-L["Set Scale"] = "Set Scale"
-L["Sets Scale of the Micro Bar"] = "Sets Scale of the Micro Bar"
-L["As Letters"] = true
-L["Replace icons with just letters.\n|cffFF0000Warning:|r this will disable original Blizzard's tooltips for microbar."] = true
--- ElvUI_Microbar/locales/russian.lua
+++
@@ -1,9 +0,0 @@
-﻿--Файл локализации для ruRU
-local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
-local L = AceLocale:NewLocale("ElvUI", "ruRU")
-if not L then return; end
-
-L["Set Scale"] = "Масштаб"
-L["Sets Scale of the Micro Bar"] = "Устанавливает масштаб микроменю"
-L["As Letters"] = "В виде букв"
-L["Replace icons with just letters.\n|cffFF0000Warning:|r this will disable original Blizzard's tooltips for microbar."] = "Заменить иконки буквами.\n|cffFF0000Внимание:|r это отключит оригинальные подсказки микроменю от Blizzard."
