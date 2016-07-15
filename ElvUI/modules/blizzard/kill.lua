f755ab248d6f25aafaa218c62f211c2de4664c7e
Appolyonn <jellefenxd@gmail.com>
2016-07-15 03:24:12 +0300

    Update Kill BLizz

--- ElvUI/modules/blizzard/kill.lua
+++ ElvUI/modules/blizzard/kill.lua
@@ -1,33 +1,6 @@
 local E, L, DF = unpack(select(2, ...))
 local B = E:GetModule('Blizzard');
 
-local removeMenuOptions = {
-	["SET_FOCUS"] = true,
-	["CLEAR_FOCUS"] = true,
-}
-
-local function DisableButtons(self)
-	if not self.numButtons then return; end
-	local lastButton
-	local numButtons = 0
-	for i=1, self.numButtons do
-		local button = _G[self:GetName()..'Button'..i]
-		if removeMenuOptions[button.value] then
-			button:Hide()
-
-			for z = i + 1, self.numButtons do
-				local point, attachTo, anchorPoint, x, y = _G[self:GetName()..'Button'..z]:GetPoint()
-				_G[self:GetName()..'Button'..z]:Point(point, attachTo, anchorPoint, x, y + UIDROPDOWNMENU_BUTTON_HEIGHT);
-			end
-			numButtons = numButtons + 1
-		end
-
-		lastButton = button
-	end
-
-	self:Height(self:GetHeight() - (UIDROPDOWNMENU_BUTTON_HEIGHT * numButtons))
-end
-
 function B:KillBlizzard()
-	DropDownList1:HookScript('OnShow', DisableButtons)
+
 end
