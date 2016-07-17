64d72801182faa81441388db14e6f7decefc6ccf
Appolyonn <jellefenxd@gmail.com>
2016-07-17 13:30:24 +0300

    Revert "World State Frame"
    This reverts commit 3adcff6df553417e8773fd4cf7c70f90d472f057.

--- ElvUI/modules/blizzard/capturebar.lua
+++ ElvUI/modules/blizzard/capturebar.lua
@@ -15,31 +15,6 @@ function B:WorldStateAlwaysUpFrame_Update()
 	end
 end
 
-hooksecurefunc("WorldStateAlwaysUpFrame_Update", function()
-	WorldStateAlwaysUpFrame:ClearAllPoints()
-	WorldStateAlwaysUpFrame:SetPoint("TOP", UIParent, "TOP", 0, 0)
-
-	local _, _, _, _, y = AlwaysUpFrame1:GetPoint()
-	AlwaysUpFrame1:SetPoint("TOP", WorldStateAlwaysUpFrame, "TOP", 0, y)
-
-	for i = 1, NUM_ALWAYS_UP_UI_FRAMES do
-		local frame = _G["AlwaysUpFrame"..i]
-	
-		local text = _G["AlwaysUpFrame"..i.."Text"]
-		text:ClearAllPoints()
-		text:SetPoint("CENTER", frame, "CENTER", 0, 0)
-		text:SetJustifyH("CENTER")
-	
-		local icon = _G["AlwaysUpFrame"..i.."Icon"]
-		icon:ClearAllPoints()
-		icon:SetPoint("RIGHT", text, "LEFT", 10, -8)
-
-		local dynamicIcon = _G["AlwaysUpFrame"..i.."DynamicIconButton"]
-		dynamicIcon:ClearAllPoints()
-		dynamicIcon:SetPoint("LEFT", text, "RIGHT", 5, 0)
-	end
-end)
-
 function B:PositionCaptureBar()
 	self:SecureHook("WorldStateAlwaysUpFrame_Update");
 end
