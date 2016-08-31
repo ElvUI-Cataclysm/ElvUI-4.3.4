8d5c1eb0e79d40b7017c924ed51645bb1326a302
Appolyonn <jellefenxd@gmail.com>
2016-08-31 11:37:12 +0300

    Custom Timestamp Color Added

--- ElvUI/config/profile.lua
+++ ElvUI/config/profile.lua
@@ -2318,6 +2318,8 @@ P['chat'] = {
 	["useAltKey"] = false,
 	["classColorMentionsChat"] = true,
 	["numAllowedCombatRepeat"] = 3,
+	["useCustomTimeColor"] = true,
+	["customTimeColor"] = {r = 0.7, g = 0.7, b = 0.7},
 	
 	['tabFont'] = 'PT Sans Narrow',
 	['tabFontSize'] = 12,
