5e54e3bbc9bb9931be7d49dbb1a73b7c9274a20d
Appolyonn <jellefenxd@gmail.com>
2016-11-03 19:36:58 +0200

    Update aurabars

--- ElvUI/modules/unitframes/elements/aurabars.lua
+++ ElvUI/modules/unitframes/elements/aurabars.lua
@@ -14,7 +14,7 @@ local RAID_CLASS_COLORS = RAID_CLASS_COLORS;
 
 function UF:Construct_AuraBars()
 	local bar = self.statusBar;
-
+	auraBar:SetFrameLevel(frame.RaisedElementParent:GetFrameLevel() + 10) --Make them appear above any text element
 	self:SetTemplate("Default", nil, nil, UF.thinBorders);
 	local inset = UF.thinBorders and E.mult or nil;
 	bar:SetInside(self, inset, inset);
