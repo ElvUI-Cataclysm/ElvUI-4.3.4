0eeea38e5effbba7e353f3a1a8fdde44922eb91b
Appolyonn <jellefenxd@gmail.com>
2016-12-15 07:22:40 +0200

    Update

--- ElvUI/modules/skins/blizzard/bgscore.lua
+++ ElvUI/modules/skins/blizzard/bgscore.lua
@@ -1,5 +1,5 @@
 local E, L, V, P, G = unpack(select(2, ...));
-local S = E:GetModule('Skins')
+local S = E:GetModule("Skins")
 
 local split = string.split;
 
@@ -7,24 +7,19 @@ local function LoadSkin()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.bgscore ~= true then return end
 
 	for i = 19, MAX_WORLDSTATE_SCORE_BUTTONS do
-		_G['WorldStateScoreButton'..i]:StripTextures()
+		_G["WorldStateScoreButton"..i]:StripTextures()
 	end
 	MAX_WORLDSTATE_SCORE_BUTTONS = 18; WorldStateScoreFrame_Resize()
 
-	WorldStateScoreScrollFrame:StripTextures()
 	WorldStateScoreFrame:StripTextures()
 	WorldStateScoreFrame:SetTemplate("Transparent")
+
+	WorldStateScoreScrollFrame:StripTextures()
+	WorldStateScoreFrameInset:Kill()
+
 	S:HandleCloseButton(WorldStateScoreFrameCloseButton)
 	S:HandleScrollBar(WorldStateScoreScrollFrameScrollBar)
-	WorldStateScoreFrameInset:Kill()
 	S:HandleButton(WorldStateScoreFrameLeaveButton)
-
-	for i = 1, WorldStateScoreScrollFrameScrollChildFrame:GetNumChildren() do
-		local b = _G["WorldStateScoreButton"..i]
-		b:StripTextures()
-		b:StyleButton(false)
-		b:SetTemplate("Default", true)
-	end
 
 	for i = 1, 3 do 
 		S:HandleTab(_G["WorldStateScoreFrameTab"..i])
