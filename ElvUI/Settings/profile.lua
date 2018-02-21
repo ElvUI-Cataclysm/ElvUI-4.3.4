e174ceaccda2733d725a68ebab331b27e0c6f946
Apollyonn <jellefenxd@gmail.com>
2018-02-21 19:13:35 +0200

    Added color options for Debuff Highlighting on unitframes

--- ElvUI/Settings/profile.lua
+++ ElvUI/Settings/profile.lua
@@ -728,6 +728,12 @@ P["unitframe"] = {
 				[2] = {r = .80, g = .82, b = .60}
 			},
 			["PRIEST"] = {r = 1, g = 1, b = 1}
+		},
+		["debuffHighlight"] = {
+			["Magic"] = {r = 0.2, g = 0.6, b = 1, a = 0.45},
+			["Curse"] = {r = 0.6, g = 0, b = 1, a = 0.45},
+			["Disease"] = {r = 0.6, g = 0.4, b = 0, a = 0.45},
+			["Poison"] = {r = 0, g = 0.6, b = 0, a = 0.45}
 		}
 	},
 
