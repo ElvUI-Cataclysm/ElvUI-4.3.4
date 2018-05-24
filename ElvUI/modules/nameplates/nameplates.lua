b694c29c90c0758cd2db8eb42677b7fea2c79b51
Apollyonn <jellefenxd@gmail.com>
2018-05-24 21:56:31 +0300

    update

--- ElvUI/modules/nameplates/nameplates.lua
+++ ElvUI/modules/nameplates/nameplates.lua
@@ -429,6 +429,7 @@ function mod:OnHide()
 	self.UnitFrame.RightArrow:Hide()
 	self.UnitFrame.HealthBar.r, self.UnitFrame.HealthBar.g, self.UnitFrame.HealthBar.b = nil, nil, nil
 	self.UnitFrame.HealthBar:Hide()
+	self.UnitFrame.HealthBar.currentScale = nil
 	self.UnitFrame.oldCastBar:Hide()
 	self.UnitFrame.CastBar:Hide()
 	self.UnitFrame.Level:ClearAllPoints()
