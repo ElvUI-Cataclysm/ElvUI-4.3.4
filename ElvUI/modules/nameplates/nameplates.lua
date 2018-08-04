d22f99cb57c5c2dac46ce3e49916d2594c778a2f
Apollyonn <jellefenxd@gmail.com>
2018-08-04 13:31:40 +0300

    update

--- ElvUI/modules/nameplates/nameplates.lua
+++ ElvUI/modules/nameplates/nameplates.lua
@@ -635,7 +635,7 @@ function mod:QueueObject(object)
 	object:Hide()
 end
 
-function mod:OnUpdate(elapsed)
+function mod:OnUpdate()
 	local count = select("#", WorldGetChildren(WorldFrame))
 	if count ~= numChildren then
 		local frame, region
