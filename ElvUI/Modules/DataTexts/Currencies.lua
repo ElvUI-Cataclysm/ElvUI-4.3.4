5b62e77b3e8416cc8bb924084a7c3ea0b77b6440
Apollyonn <Silentchaos@hotmail.gr>
2020-04-02 14:12:11 +0300

    currencies datatext

--- ElvUI/Modules/DataTexts/Currencies.lua
+++ ElvUI/Modules/DataTexts/Currencies.lua
@@ -1,8 +1,8 @@
 local E, L, V, P, G = unpack(select(2, ...))
 local DT = E:GetModule("DataTexts")
 
-local select, pairs = select, pairs
-local format, join = string.format, string.join
+local pairs = pairs
+local format = string.format
 
 local GetCurrencyInfo = GetCurrencyInfo
 local GetMoney = GetMoney
@@ -11,49 +11,52 @@ local CURRENCY, OTHER, PVP = CURRENCY, OTHER, PVP
 local EXPANSION_NAME3 = EXPANSION_NAME3
 local COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATEPVE = COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATEPVE
 
-local IconPath = "Interface\\Icons\\"
-local iconString = "\124T%s:%d:%d:0:0:64:64:4:60:4:60\124t"
+local iconPath = "Interface\\Icons\\"
+local iconString = "\124T%s%s:%d:%d:0:0:64:64:4:60:4:60\124t"
 
 local currencyList, gold
 
-local function getCurrencyInfo(id)
-	local name, count = GetCurrencyInfo(id)
-
-	return name, count
+local function getCurrencyCount(id)
+	local _, count = GetCurrencyInfo(id)
+	return count
 end
 
 local function AddTexture(texture)
-	local icon = format(iconString, IconPath..texture, 14, 14)
-
-	return icon
+	return format(iconString, iconPath, texture, 14, 14)
 end
 
-local Currencies = {
+local currencies = {
 	-- PvP
-	["392"] = {ID = 392, NAME = getCurrencyInfo(392), ICON = AddTexture("PVPCurrency-Honor-"..E.myfaction)},	-- Honor Points
-	["390"] = {ID = 390, NAME = getCurrencyInfo(390), ICON = AddTexture("PVPCurrency-Conquest-"..E.myfaction)},	-- Conquest points
+	["392"] = {ID = 392, NAME = GetCurrencyInfo(392), ICON = AddTexture("PVPCurrency-Honor-"..E.myfaction)},	-- Honor Points
+	["390"] = {ID = 390, NAME = GetCurrencyInfo(390), ICON = AddTexture("PVPCurrency-Conquest-"..E.myfaction)},	-- Conquest points
 	-- PvE
-	["395"] = {ID = 395, NAME = getCurrencyInfo(395), ICON = AddTexture("pvecurrency-justice")},				-- Justice Points
-	["396"] = {ID = 396, NAME = getCurrencyInfo(396), ICON = AddTexture("pvecurrency-valor")},					-- Valor Points
+	["395"] = {ID = 395, NAME = GetCurrencyInfo(395), ICON = AddTexture("pvecurrency-justice")},				-- Justice Points
+	["396"] = {ID = 396, NAME = GetCurrencyInfo(396), ICON = AddTexture("pvecurrency-valor")},					-- Valor Points
 	-- Cata
-	["402"] = {ID = 402, NAME = getCurrencyInfo(402), ICON = AddTexture("achievement_profession_chefhat")},		-- Chef's Award
-	["515"] = {ID = 515, NAME = getCurrencyInfo(515), ICON = AddTexture("inv_misc_ticket_darkmoon_01")},		-- Darkmoon Prize Ticket
-	["361"] = {ID = 361, NAME = getCurrencyInfo(361), ICON = AddTexture("inv_misc_token_argentdawn3")},			-- Illustrious jewelcrafter's Token
-	["416"] = {ID = 416, NAME = getCurrencyInfo(416), ICON = AddTexture("inv_misc_markoftheworldtree")},		-- Mark of the World Tree
-	["614"] = {ID = 614, NAME = getCurrencyInfo(614), ICON = AddTexture("spell_shadow_sealofkings")},			-- Mote of Darkness
-	["391"] = {ID = 391, NAME = getCurrencyInfo(391), ICON = AddTexture("Achievement_Zone_TolBarad")},			-- Tol Barad Commendation
+	["402"] = {ID = 402, NAME = GetCurrencyInfo(402), ICON = AddTexture("achievement_profession_chefhat")},		-- Chef's Award
+	["515"] = {ID = 515, NAME = GetCurrencyInfo(515), ICON = AddTexture("inv_misc_ticket_darkmoon_01")},		-- Darkmoon Prize Ticket
+	["361"] = {ID = 361, NAME = GetCurrencyInfo(361), ICON = AddTexture("inv_misc_token_argentdawn3")},			-- Illustrious jewelcrafter's Token
+	["416"] = {ID = 416, NAME = GetCurrencyInfo(416), ICON = AddTexture("inv_misc_markoftheworldtree")},		-- Mark of the World Tree
+	["614"] = {ID = 614, NAME = GetCurrencyInfo(614), ICON = AddTexture("spell_shadow_sealofkings")},			-- Mote of Darkness
+	["391"] = {ID = 391, NAME = GetCurrencyInfo(391), ICON = AddTexture("Achievement_Zone_TolBarad")},			-- Tol Barad Commendation
 	-- Other
-	["081"] = {ID = 081, NAME = getCurrencyInfo(81),  ICON = AddTexture("INV_Misc_Ribbon_01")},					-- Dalaran Cooking Award
-	["061"] = {ID = 061, NAME = getCurrencyInfo(61),  ICON = AddTexture("INV_Misc_Gem_Variety_01")},			-- Dalaran Jewelcrafter's Token
-	["126"] = {ID = 126, NAME = getCurrencyInfo(126), ICON = AddTexture("INV_Jewelry_Ring_66")},				-- Wintergrasp Mark of Honor
+	["081"] = {ID = 081, NAME = GetCurrencyInfo(81),  ICON = AddTexture("INV_Misc_Ribbon_01")},					-- Dalaran Cooking Award
+	["061"] = {ID = 061, NAME = GetCurrencyInfo(61),  ICON = AddTexture("INV_Misc_Gem_Variety_01")},			-- Dalaran Jewelcrafter's Token
+	["126"] = {ID = 126, NAME = GetCurrencyInfo(126), ICON = AddTexture("INV_Jewelry_Ring_66")},				-- Wintergrasp Mark of Honor
 }
 
+for _, data in pairs(currencies) do
+	data.TEXT = format("%s %s", data.ICON, data.NAME)
+end
+
 function DT:Currencies_GetCurrencyList()
-	currencyList = {}
-	for currency, data in pairs(Currencies) do
-		currencyList[currency] = data.NAME
+	if not currencyList then
+		currencyList = {}
+		for currency, data in pairs(currencies) do
+			currencyList[currency] = data.NAME
+		end
+		currencyList.GOLD = L["Gold"]
 	end
-	currencyList.GOLD = L["Gold"]
 
 	return currencyList
 end
@@ -64,17 +67,15 @@ local function OnEvent(self)
 	if E.db.datatexts.currencies.displayedCurrency == "GOLD" then
 		self.text:SetText(E:FormatMoney(gold, E.db.datatexts.goldFormat or "BLIZZARD", not E.db.datatexts.goldCoins))
 	else
-		local chosenCurrency = Currencies[E.db.datatexts.currencies.displayedCurrency]
+		local chosenCurrency = currencies[E.db.datatexts.currencies.displayedCurrency]
 
 		if chosenCurrency then
-			local count = select(2, getCurrencyInfo(chosenCurrency.ID))
-
 			if E.db.datatexts.currencies.displayStyle == "ICON" then
-				self.text:SetFormattedText("%s %d", chosenCurrency.ICON, count)
+				self.text:SetFormattedText("%s %d", chosenCurrency.ICON, getCurrencyCount(chosenCurrency.ID))
 			elseif E.db.datatexts.currencies.displayStyle == "ICON_TEXT" then
-				self.text:SetFormattedText("%s %s %d", chosenCurrency.ICON, chosenCurrency.NAME, count)
+				self.text:SetFormattedText("%s %s %d", chosenCurrency.ICON, chosenCurrency.NAME, getCurrencyCount(chosenCurrency.ID))
 			else
-				self.text:SetFormattedText("%s %s %d", chosenCurrency.ICON, E:AbbreviateString(chosenCurrency.NAME), count)
+				self.text:SetFormattedText("%s %s %d", chosenCurrency.ICON, E:AbbreviateString(chosenCurrency.NAME), getCurrencyCount(chosenCurrency.ID))
 			end
 		end
 	end
@@ -88,28 +89,28 @@ local function OnEnter(self)
 	DT.tooltip:AddLine(" ")
 	-- PvP
 	DT.tooltip:AddLine(PVP)
-	DT.tooltip:AddDoubleLine(join("", Currencies["392"].ICON, " ", Currencies["392"].NAME), select(2, getCurrencyInfo(392)), 1, 1, 1)	-- Honor Points
-	DT.tooltip:AddDoubleLine(join("", Currencies["390"].ICON, " ", Currencies["390"].NAME), select(2, getCurrencyInfo(390)), 1, 1, 1)	-- Conquest points
+	DT.tooltip:AddDoubleLine(currencies["392"].TEXT, getCurrencyCount(392), 1, 1, 1)	-- Honor Points
+	DT.tooltip:AddDoubleLine(currencies["390"].TEXT, getCurrencyCount(390), 1, 1, 1)	-- Conquest points
 	DT.tooltip:AddLine(" ")
 	-- PvE
 	DT.tooltip:AddLine(COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATEPVE)
-	DT.tooltip:AddDoubleLine(join("", Currencies["395"].ICON, " ", Currencies["395"].NAME), select(2, getCurrencyInfo(395)), 1, 1, 1)	-- Justice Points
-	DT.tooltip:AddDoubleLine(join("", Currencies["396"].ICON, " ", Currencies["396"].NAME), select(2, getCurrencyInfo(396)), 1, 1, 1)	-- Valor Points
+	DT.tooltip:AddDoubleLine(currencies["395"].TEXT, getCurrencyCount(395), 1, 1, 1)	-- Justice Points
+	DT.tooltip:AddDoubleLine(currencies["396"].TEXT, getCurrencyCount(396), 1, 1, 1)	-- Valor Points
 	DT.tooltip:AddLine(" ")
 	-- Cata
 	DT.tooltip:AddLine(EXPANSION_NAME3)
-	DT.tooltip:AddDoubleLine(join("", Currencies["402"].ICON, " ", Currencies["402"].NAME), select(2, getCurrencyInfo(402)), 1, 1, 1)	-- Chef's Award
-	DT.tooltip:AddDoubleLine(join("", Currencies["515"].ICON, " ", Currencies["515"].NAME), select(2, getCurrencyInfo(515)), 1, 1, 1)	-- Darkmoon Prize Ticket
-	DT.tooltip:AddDoubleLine(join("", Currencies["361"].ICON, " ", Currencies["361"].NAME), select(2, getCurrencyInfo(361)), 1, 1, 1)	-- Illustrious jewelcrafter's Token
-	DT.tooltip:AddDoubleLine(join("", Currencies["416"].ICON, " ", Currencies["416"].NAME), select(2, getCurrencyInfo(416)), 1, 1, 1)	-- Mark of the World Tree
-	DT.tooltip:AddDoubleLine(join("", Currencies["614"].ICON, " ", Currencies["614"].NAME), select(2, getCurrencyInfo(614)), 1, 1, 1)	-- Mote of Darkness
-	DT.tooltip:AddDoubleLine(join("", Currencies["391"].ICON, " ", Currencies["391"].NAME), select(2, getCurrencyInfo(391)), 1, 1, 1)	-- Tol Barad Commendation
+	DT.tooltip:AddDoubleLine(currencies["402"].TEXT, getCurrencyCount(402), 1, 1, 1)	-- Chef's Award
+	DT.tooltip:AddDoubleLine(currencies["515"].TEXT, getCurrencyCount(515), 1, 1, 1)	-- Darkmoon Prize Ticket
+	DT.tooltip:AddDoubleLine(currencies["361"].TEXT, getCurrencyCount(361), 1, 1, 1)	-- Illustrious jewelcrafter's Token
+	DT.tooltip:AddDoubleLine(currencies["416"].TEXT, getCurrencyCount(416), 1, 1, 1)	-- Mark of the World Tree
+	DT.tooltip:AddDoubleLine(currencies["614"].TEXT, getCurrencyCount(614), 1, 1, 1)	-- Mote of Darkness
+	DT.tooltip:AddDoubleLine(currencies["391"].TEXT, getCurrencyCount(391), 1, 1, 1)	-- Tol Barad Commendation
 	DT.tooltip:AddLine(" ")
 	-- Other
 	DT.tooltip:AddLine(OTHER)
-	DT.tooltip:AddDoubleLine(join("", Currencies["081"].ICON, " ", Currencies["081"].NAME), select(2, getCurrencyInfo(081)), 1, 1, 1)	-- Dalaran Cooking Award
-	DT.tooltip:AddDoubleLine(join("", Currencies["061"].ICON, " ", Currencies["061"].NAME), select(2, getCurrencyInfo(061)), 1, 1, 1)	-- Dalaran Jewelcrafter's Token
-	DT.tooltip:AddDoubleLine(join("", Currencies["126"].ICON, " ", Currencies["126"].NAME), select(2, getCurrencyInfo(126)), 1, 1, 1)	-- Wintergrasp Mark of Honor
+	DT.tooltip:AddDoubleLine(currencies["081"].TEXT, getCurrencyCount(081), 1, 1, 1)	-- Dalaran Cooking Award
+	DT.tooltip:AddDoubleLine(currencies["061"].TEXT, getCurrencyCount(061), 1, 1, 1)	-- Dalaran Jewelcrafter's Token
+	DT.tooltip:AddDoubleLine(currencies["126"].TEXT, getCurrencyCount(126), 1, 1, 1)	-- Wintergrasp Mark of Honor
 
 	DT.tooltip:Show()
 end
