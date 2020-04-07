09c30c4327155a42594c78397560503a7ee5b35c
Apollyonn <Silentchaos@hotmail.gr>
2020-04-07 18:38:45 +0300

    cleanup

--- ElvUI/Modules/Skins/Blizzard/Worldmap.lua
+++ ElvUI/Modules/Skins/Blizzard/Worldmap.lua
@@ -1,8 +1,6 @@
 local E, L, V, P, G = unpack(select(2, ...))
 local S = E:GetModule("Skins")
 
-local _G = _G
-
 local function LoadSkin()
 	if not E.private.skins.blizzard.enable or not E.private.skins.blizzard.worldmap then return end
 
