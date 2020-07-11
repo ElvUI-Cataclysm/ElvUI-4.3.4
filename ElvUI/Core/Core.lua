3b0cd5ef55dc1fa692cea5b773e4492a894cb7d4
Apollyonn <Silentchaos@hotmail.gr>
2020-07-11 04:53:17 +0300

    cleanup

--- ElvUI/Core/Core.lua
+++ ElvUI/Core/Core.lua
@@ -7,7 +7,7 @@ local _G = _G
 local tonumber, pairs, ipairs, error, unpack, select, tostring = tonumber, pairs, ipairs, error, unpack, select, tostring
 local assert, rawget, rawset, setmetatable, type = assert, rawget, rawset, setmetatable, type
 local twipe, tinsert, tremove, next = table.wipe, tinsert, tremove, next
-local format, find, match, strrep, strlen, sub, gsub, strjoin, strsplit = string.format, string.find, string.match, strrep, strlen, string.sub, string.gsub, strjoin, string.split
+local format, find, match, strrep, strlen, sub, gsub, strjoin = string.format, string.find, string.match, strrep, strlen, string.sub, string.gsub, strjoin
 
 local UnitGUID = UnitGUID
 local CreateFrame = CreateFrame
